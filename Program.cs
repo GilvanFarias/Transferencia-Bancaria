0772d31e34e787757d6c9a882eb9378c598454ba
Gilvan Farias <63085957+GilvanFarias@users.noreply.github.com>
2021-03-04 20:33:18 -0300

    Iniciando os primeiros passos com o projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplicacao_Transferencia_Bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
